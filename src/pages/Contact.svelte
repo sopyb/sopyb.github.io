590e7a10798b927994fe2d90e983d8ec7c5d8cc9
sopy <doimpt@sopy.one>
2023-01-17 18:09:12 +0200

    Contact page (for now)

--- src/pages/Contact.svelte
+++ src/pages/Contact.svelte
@@ -8,3 +8,58 @@
     dispatch('mounted', true)
   })
 </script>
+
+<Panel bgcolor="#222">
+    <div class="parent">
+        <h1>Where can you find me?</h1>
+        <div class="socialGrid">
+            <div>
+                <a href="mailto:contact@sopy.one">
+                    <i class="nf nf-cod-mail"></i> Email
+                </a>
+            </div>
+            <div>
+                <a href="https://instagram.com/doimptsopy">
+                    <i class="nf nf-fa-instagram"></i> Instagram
+                </a>
+            </div>
+            <div>
+                <a href="https://github.com/sopyb">
+                    <i class="nf nf-fa-github"></i> Github
+                </a>
+            </div>
+        </div>
+    </div>
+</Panel>
+
+<style>
+    .parent {
+        margin: 0 4vw;
+    }
+
+    .socialGrid {
+        display: grid;
+        grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+        grid-gap: 1rem;
+    }
+
+    .socialGrid div {
+        background-color: #333;
+        border-radius: 5px;
+    }
+
+    .socialGrid div:hover {
+        background-color: #444;
+    }
+
+    .socialGrid div a {
+        display: block;
+        color: #fff;
+        text-decoration: none;
+        font-size: 1rem;
+        padding: 1rem;
+
+        width: 100%;
+        height: 100%;
+    }
+</style>
