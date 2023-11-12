999b19dcbd02b5a68b260ee0719e002dae7074cc
sopy <contact@sopy.one>
2023-11-12 22:00:08 +0200

    Give windows keys so svelte knows what windows to clear

--- src/components/desktop/windowing/WindowManager.svelte
+++ src/components/desktop/windowing/WindowManager.svelte
@@ -12,7 +12,7 @@
 <div
         class="top-0 left-0 w-full h-full"
 >
-    {#each $windows as desktopWindow}
+    {#each $windows as desktopWindow (desktopWindow.id)}
         <div in:fade={{duration: 100, delay: 500}}
                 out:fade={{duration: 100, delay: 0}}
         >
